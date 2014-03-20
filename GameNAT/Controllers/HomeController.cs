dd7e8270b248a2bdd1cbec516a99e21df90b2eae
Steve McAllister <steve.mcallister92@gmail.com>
2014-03-19 20:54:10 -0500

    Uploading files

---
+++ GameNAT/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameNAT.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly string apiKey = "b09b6521e4db2bbbaf82fc09dfea64c1";
+		private XboxVotingService.XboxVotingServiceSoap xboxClient = new XboxVotingService.XboxVotingServiceSoapClient("XboxVotingServiceSoap");
+
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "GameNAT is an XBox 360 game management system written for The Nerdery by Steve McAllister.";
+
+			return View();
+		}
+
+		public ActionResult GetGames()
+		{
+			return View();
+		}
+	}
+}
---
+++ GameNAT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameNAT.MvcApplication" Language="C#" %>
