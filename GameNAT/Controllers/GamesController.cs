aac6d9147f9ef463bec09754b0ba7beb257676df
Steve McAllister <steve.mcallister92@gmail.com>
2014-03-20 19:46:07 -0500

    Added ClearGames, SetGotIt, and error handling.

--- GameNAT/Controllers/GamesController.cs
+++ GameNAT/Controllers/GamesController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using GameNAT.Models;
+using System;
 
 namespace GameNAT.Controllers
 {
-    public class GamesController : Controller
-    {
-        private GamesContext db = new GamesContext();
+	public class GamesController : Controller
+	{
+		private GamesContext db = new GamesContext();
 
 		// GET: /Games/AddGame/
 		/// <summary>
@@ -27,17 +28,133 @@ namespace GameNAT.Controllers
 		[HttpPost]
 		public ViewResult AddGame(string title)
 		{
-			db.AddGame(title);
-			return View();
+			try
+			{
+				if (db.AddGame(title))
+				{
+					// If we are able to add the new title, let the user know.
+					ViewBag.StatusMessage = string.Format("Game {0} added successfully.", title);
+				}
+				else
+				{
+					// Otherwise, let the user know the title couldn't be added.
+					ViewBag.StatusMessage = string.Format("Unable to add the game {0}.  Please try again.", title);
+				}
+				return View();
+			}
+			catch
+			{
+				// An error has occured and the user needs to be notified.
+				ViewBag.StatusMessage = "An error has occured.  Please try again.";
+				return View();
+			}
 		}
 
 		// GET: /Games/AddVote/
+		/// <summary>
+		/// Displays the add vote page.
+		/// </summary>
+		/// <returns>The add vote view.</returns>
+		[HttpGet]
+		public ActionResult AddVote()
+		{
+			return View();
+		}
+
+		/// <summary>
+		/// Adds a vote to the specified id.
+		/// </summary>
+		/// <param name="id">The id of the game.</param>
+		/// <returns></returns>
+		[HttpPost]
+		public ViewResult AddVote(int id)
+		{
+			try
+			{
+				if (db.AddVote(id))
+				{
+					// If we are able to find the id and a vote has been added, let the user know.
+					ViewBag.StatusMessage = string.Format("Vote added successfully to game {0}.", id);
+				}
+				else
+				{
+					// Otherwise, let the user know the id couldn't be found.
+					ViewBag.StatusMessage = string.Format("Unable to find the game id {0}.  Please try again.", id);
+				}
+				return View();
+			}
+			catch
+			{
+				// An error has occured and the user needs to be notified.
+				ViewBag.StatusMessage = string.Format("Unable to find game id {0}.  Please try again.", id);
+				return View();
+			}
+		}
+
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <returns></returns>
 		[HttpGet]
-		public ActionResult AddVote()
+		public ActionResult ClearGames()
+		{
+			return View();
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[HttpPost]
+		public ActionResult ClearGames(bool clear = true)
+		{
+			try
+			{
+				if (db.ClearGames())
+				{
+					// If we are able to clear all games, let the user know.
+					ViewBag.StatusMessage = "All games have been cleared.";
+				}
+				else
+				{
+					// Otherwise, let the user know it couldn't be done.
+					ViewBag.StatusMessage = "Unable to clear all games.  Please try again.";
+				}
+				return View();
+			}
+			catch
+			{
+				// An error has occured and the user needs to be notified.
+				ViewBag.StatusMessage = "An error has occured.  Please try again.";
+				return View();
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		// GET: /Games/
+		/// <summary>
+		/// Displays the games page with a list of the current games.
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult Index()
+		{
+			return View(db.GetGames().ToList());
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		public ActionResult SetGotIt()
 		{
 			return View();
 		}
@@ -48,25 +165,28 @@ namespace GameNAT.Controllers
 		/// <param name="id"></param>
 		/// <returns></returns>
 		[HttpPost]
-		public ViewResult AddVote(int id)
+		public ActionResult SetGotIt(int id)
 		{
-			db.AddVote(id);
-			return View();
+			try
+			{
+				if (db.SetGotIt(id))
+				{
+					// If we are able to find the id and set got it, let the user know.
+					ViewBag.StatusMessage = string.Format("Set got it successfully for game {0}.", id);
+				}
+				else
+				{
+					// Otherwise, let the user know the id couldn't be found.
+					ViewBag.StatusMessage = string.Format("Unable to set got it for the game id {0}.  Please try again.", id);
+				}
+				return View();
+			}
+			catch
+			{
+				// An error has occured and the user needs to be notified.
+				ViewBag.StatusMessage = string.Format("Unable to find game id {0}.  Please try again.", id);
+				return View();
+			}
 		}
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-		// GET: /Games/
-		public ActionResult Index()
-		{
-			return View(db.GetGames().ToList());
-		}
-    }
+	}
 }
--- GameNAT/Models/GamesContext.cs
+++ GameNAT/Models/GamesContext.cs
@@ -6,39 +6,43 @@ using System.Web;
 
 namespace GameNAT.Models
 {
-    public class GamesContext : DbContext
-    {
+	public class GamesContext : DbContext
+	{
 		private readonly string apiKey = "b09b6521e4db2bbbaf82fc09dfea64c1";
 		private XboxVotingService.XboxVotingServiceSoap xboxClient = new XboxVotingService.XboxVotingServiceSoapClient("XboxVotingServiceSoap");
 
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, please use data migrations.
-        // For more information refer to the documentation:
-        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
-        public GamesContext() : base("name=GamesContext")
-        {
+		public GamesContext() : base("name=GamesContext")
+		{
 
-        }
+		}
 
 		/// <summary>
 		/// Adds a new game with the specified title.
 		/// </summary>
 		/// <param name="title">The title of the game to add.</param>
-		public void AddGame(string title)
+		/// <returns></returns>
+		public bool AddGame(string title)
 		{
-			xboxClient.AddGame(title, apiKey);
+			return xboxClient.AddGame(title, apiKey);
 		}
 
 		/// <summary>
 		/// Adds a vote to the game with the specified id.
 		/// </summary>
 		/// <param name="id">The id of the game to add a vote for.</param>
-		public void AddVote(int id)
+		/// <returns></returns>
+		public bool AddVote(int id)
 		{
-			xboxClient.AddVote(id, apiKey);
+			return xboxClient.AddVote(id, apiKey);
+		}
+
+		/// <summary>
+		/// Clears the games collection.
+		/// </summary>
+		/// <returns></returns>
+		public bool ClearGames()
+		{
+			return xboxClient.ClearGames(apiKey);
 		}
 
 		/// <summary>
@@ -50,7 +54,23 @@ namespace GameNAT.Models
 			return xboxClient.GetGames(apiKey);
 		}
 
-		public DbSet<GameNAT.XboxVotingService.XboxGame> XboxGames { get; set; }
-    
-    }
+		/// <summary>
+		/// Sets the specified game id to got it.
+		/// </summary>
+		/// <param name="id">The id of the game.</param>
+		/// <returns></returns>
+		public bool SetGotIt(int id)
+		{
+			return xboxClient.SetGotIt(id, apiKey);
+		}
+
+		/// <summary>
+		/// Gets or sets the XBox games data context.
+		/// </summary>
+		public DbSet<GameNAT.XboxVotingService.XboxGame> XboxGames
+		{
+			get;
+			set;
+		}
+	}
 }
